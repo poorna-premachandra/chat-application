245356f47aa268ab6def826b4f622ca8c21dd726
Poorna Premachandra <17600522+poorna-premachandra@users.noreply.github.com>
2022-07-01 00:06:50 +0530

    Dynamically add table rows

--- frontend/src/component/Client.js
+++ frontend/src/component/Client.js
@@ -4,12 +4,14 @@ import "../css/client.css"
 const client = function Client() {
     const [connectButtonValue, setValueConnectButton] = useState(false);
     const [disconnectButtonValue, setValueDisconnectButton] = useState(true);
+    const [chat, setChat] = useState("");
+    const [chatHistory, setChatHistory] = useState([]);
 
     return (
         <div className="main">
             <h1>Chat Application</h1>
             <div className="panel">
-                <div className="connection-panel">
+                <div className="panel__connection-panel button">
                     <label>Websocket Connection :</label>
                     <button id="connect" className="btn btn-default" type="text"
                             onClick={() => onSubmitConnect(setValueConnectButton, setValueDisconnectButton)}
@@ -20,12 +22,27 @@ const client = function Client() {
                             disabled={disconnectButtonValue}>Disconnect
                     </button>
                 </div>
-                <div className="input-panel">
-                    <input placeholder="Type something"/>
+                <div className="panel__input-panel button">
+                    <input placeholder="Type something" onChange={(e) => setChat(e.target.value)}/>
                     <button id="submit" className="btn btn-default" type="text"
-                            disabled={disconnectButtonValue}>Submit
+                            onClick={() => sendChat(chat, chatHistory, setChatHistory)}
+                            disabled={disconnectButtonValue}>Send
                     </button>
                 </div>
+            </div>
+            <div>
+                <table className="panel__chat-table">
+                    <thead>
+                    <tr>
+                        <th className="panel__chat-table th">Chat Session</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {chatHistory.map(x => (<tr>
+                        <td className="panel__chat-table td">{x}</td>
+                    </tr>))}
+                    </tbody>
+                </table>
             </div>
         </div>
     )
@@ -45,6 +62,10 @@ const onSubmitDisconnect = (setValueConnectButton, setValueDisconnectButton) => 
     setValueDisconnectButton(true);
 
     //Disconnect WS 
+}
+
+const sendChat = (chat, chatHistory, setChatHistory) => {
+    setChatHistory([...chatHistory, chat]);
 }
 
 export default client;
